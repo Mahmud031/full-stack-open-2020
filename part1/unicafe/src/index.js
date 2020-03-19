0b8097ebc13706f5991fb7340c79012201d43a62
Atiq Mahmud <atiq.mahmud@innokasmedical.fi>
2020-03-19 12:37:38 +0200

    Part1 - Exercise 1.6

---
+++ part1/unicafe/src/index.js
@@ -0,0 +1,65 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+// Header component
+const Header = (props) => {
+    return (
+        <h1>
+            {props.header}
+        </h1>
+    )
+}
+
+// Button component
+const Button = ({ handleClick, name}) => {
+    return(
+        <button onClick={handleClick}>
+            {name}
+        </button>
+    )
+}
+
+// Display component
+const Display = ({name, value}) => {
+    return(
+        <div>
+            {name} {value}
+        </div>
+    )
+}
+
+const App = () => {
+    // save clicks of each button to own state
+    const [good, setGood] = useState(0)
+    const [neutral, setNeutral] = useState(0)
+    const [bad, setBad] = useState(0)
+
+    
+
+    const onGoodClicked = () => setGood(good + 1)
+    const onBadClicked = () => setBad(bad + 1)
+    const onNeutralClicked = () => setNeutral(neutral + 1)
+
+    return (
+        <div>
+            <Header header='give feedback' />
+            <br />
+            <div>
+                <Button handleClick={onGoodClicked} name='good' />
+                <Button handleClick={onNeutralClicked} name='neutral' />
+                <Button handleClick={onBadClicked} name='bad' />
+            </div>
+            <br />
+            <Header header='statistics' />
+            <br />
+            <Display name='good' value={good} />
+            <Display name='neutral' value={neutral} />
+            <Display name='bad' value={bad} />
+
+        </div>
+    )
+}
+
+ReactDOM.render(<App />, 
+  document.getElementById('root')
+)
